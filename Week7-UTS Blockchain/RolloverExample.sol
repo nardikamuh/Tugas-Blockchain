2d716645ae57122223880cb32649b1707bd38dd6
nardikamuh <104180922+nardikamuh@users.noreply.github.com>
2022-04-22 11:43:49 +0800

    Add files via upload

---
+++ Week7-UTS Blockchain/RolloverExample.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.1;
+
+contract RolloverExample {
+    uint8 public myUint8;
+    function decrement() public {
+        unchecked {
+            myUint8--;
+        }
+    }
+    function increment() public {
+        unchecked{
+            myUint8++;
+        }
+    }
+}
